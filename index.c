827dd455a1f4c065bfa64049f4382f72c220207f
qwertyV112 <strelocpm@gmail.com>
2015-10-11 03:09:59 +0600

    1

--- index.c
+++ index.c
@@ -1,66 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <windows.h>
-#include <process.h>
-#include <time.h>
-#include <conio.h>
+#include <ncurses.h>
 
-//
-int width =80;
-int height =25;
-//
-struct Point{
-int x;
-int y;
+struct Seting{
+        int h;
+        int w;
 };
-//8 -почтовых станцый (5-7 лошедей для одной станции)
-//лошади отдыхаит после поездки
-//На четырёх ведущих станциях, моделирующих города
-void gotoxy(struct Point t)
-{
-	COORD scrn;// = t;
-
-	HANDLE hOuput = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	scrn.X = t.x; scrn.Y = t.x;
-
-	SetConsoleCursorPosition(hOuput, scrn);
-}
-struct Loshad{
-    struct Point cord;
-    BOOL Otdix;
-};
-struct Station{
-    struct Point cord;
+struct stansh{
 };
 
-struct Station st[8];
-struct Loshad losh[8][7];
-void CreatLoshad(int k){
-    int i;
-    for(i=0;i<(5-rand()%2);i++){
-        losh[k][i].cord.x = st[k].cord.x;
-        losh[k][i].cord.y = st[k].cord.y;
+void creat(int k){
+   /* if (k>14){
+
+    }
+    else{*/
+    int i,j;
+        for(i=0;i<4;i++){
+            for(j=0;j<4;j++){
+                move(1+i*7,7+j*20);
+                addch('A');
+            }
     }
 
-}
-void CreatStation(){
-    int i;
-    for(i=0;i<8;i++){
-        st[i].cord.x = rand()%width;
-        st[i].cord.y = rand()%height;
-        CreatLoshad(i);
-    }
-}
-void print(){
-    int i;
-    for(i=0;i<4;i++){
-        printf("");
-    }
-}
 
+return;
+}
 int main()
-{
+        {
+        int c;
+        struct Seting windows;
 
+
+        printf("Р’РІРµРґРёС‚Рµ РєРѕР»РёС‡РµСЃС‚РІРѕ СЃС‚Р°РЅС†С‹Р№: ");
+        scanf("%d", &c);
+        initscr();
+        //getmaxyx(stdscr, windows.h, windows.w);
+        creat(10);
+         while((c=getch())!=27){
+        refresh();
+         usleep(30);
+         }
+         endwin();
+         //getch();
+         return 0;
 }
